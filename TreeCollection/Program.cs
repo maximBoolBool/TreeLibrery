b911c433d2bfda1035071ba49d078580d1109b0a
maximboolbool <remezmax117@gmail.com>
2023-04-01 11:28:50 +0300

    add avl tree realisation

--- TreeCollection/Program.cs
+++ TreeCollection/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using TreeCollection;
+Tree tree = new Tree();
+Node root = new Node(23,23);
+tree.Insert(22,22,root);
+tree.Insert(24,24,root);
+tree.Insert(1,1,root);
+tree.PrintTree(root);
+Console.WriteLine();
 
-Console.WriteLine("Hello, World!");
+tree.Delete(1, root);
+tree.PrintTree(root);
